559118fab988dc33690a51c5ca6ad785d2214c66
hollandcrew <49192489+hollandcrew@users.noreply.github.com>
2024-02-13 12:55:05 +0000

    DC3010-10 : signup page designed

--- training-website/src/Signup.tsx
+++ training-website/src/Signup.tsx
@@ -1,9 +1,47 @@
 import './BasicStyles.css';
+import './Signup.css';
+import Footer from './components/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilePen } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 function Signup() {
     return (
         <div>
-            <p>Hello signup page</p>
+            <div className="app-baseline-layout">
+                <header className="header">
+                    <div className="font-awesome-icon">
+                        <FontAwesomeIcon icon={faFilePen} aria-label='font-awesome' />
+                    </div>
+                    <h1>Please fill out your details to create an account</h1>
+                </header>
+                <body>
+                    <form action="">
+                        <h2>Email Address:</h2>
+                        <h4 className="subtitle">The email address you want associating with the account</h4>
+                        <div className="input-group mb-3">
+                            <input type="email" className="form-control" placeholder='EmailHandle@provider.com' aria-label='email'></input>
+                        </div>
+                        <h2>Username for site:</h2>
+                        <h4 className="subtitle">The username that will be used to login to your account</h4>
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder='User123' aria-label='username'></input>
+                        </div>
+                        <h2>Password:</h2>
+                        <h4 className="subtitle">The password for your account</h4>
+                        <div className="input-group mb-3">
+                            <input type="password" className="form-control" aria-label='password'></input>
+                        </div>
+                        <div className="input-group mb-3">
+                            <Link to="./login">
+                                <input type="submit" value="Submit"></input>
+                            </Link>
+                        </div>
+                    </form>
+
+                </body>
+            </div>
+            <Footer />
         </div>
     )
 }
